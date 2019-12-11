0a9a9983a4ac3ea310f88df8a186a904f55a0ed1
lieven <lieven.merckx@gmail.com>
2019-12-11 23:35:01 +0100

    sync with esp-idf

--- simple/src/main.cpp
+++ simple/src/main.cpp
@@ -6,7 +6,5 @@ void setup() {
 
 void loop() {
 Serial.println("[1,\"src/myTopic/time\","+String(millis())+"]");
-delay(100);
-Serial.println("[1,\"src/myTopic/build\",\"" __DATE__ " " __TIME__ "\"]");
-delay(100);
+delay(10);
 }
